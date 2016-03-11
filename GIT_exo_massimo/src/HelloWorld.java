f1eb793a8bf7008e89c3587a2e92237f47e9435d
rdebruyn <romuald.debruyne@mines-nantes.fr>
2016-03-11 15:09:03 +0100

    D

--- GIT_exo_massimo/src/HelloWorld.java
+++ GIT_exo_massimo/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello A!");
 		System.out.println("hello B and C!");
+		System.out.println("Hello D!");
 	}
 }
